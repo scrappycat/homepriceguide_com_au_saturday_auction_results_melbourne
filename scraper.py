6934f3284bb219a58a45ae1ae4f281e3637c99b8
scrappycat <mchayat+scrappycatATgithub@gmail.com>
2015-07-31 19:04:02 +1000

    Changed to page by page parsing to reduce memory footprint

--- scraper.py
+++ scraper.py
@@ -40,6 +40,73 @@ def download_file(url):
     f.close()
     return file_name
 
+def parse_page(page):
+        suburb_x0 = page.xpath('.//LTTextLineHorizontal[substring(text(),1,6)="Suburb"][1]/@x0')[0]
+        address_x0 = page.xpath('.//LTTextLineHorizontal[substring(text(),1,7)="Address"][1]/@x0')[0]
+        type_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,4)="Type"][1]/@x0')[0]
+        price_x1 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,5)="Price"][1]/@x1')[0][0:5]
+        result_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,6)="Result"][1]/@x0')[0]
+        agent_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,5)="Agent"][1]/@x0')[0]
+        lines_y0 = set(page.xpath('.//*[string-length()>0]/@y0'))
+
+        for line_y0 in lines_y0:
+
+            item = {
+                "suburb": "",
+                "address": "",
+                "type": "",
+                "price": "",
+                "result": "",
+                "agent": ""
+            }
+
+            empty_item = item.copy()
+
+            suburb = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + suburb_x0 + '"]/text()')
+            if len(suburb) > 0:
+                item["suburb"]=suburb[0].strip()
+
+            address = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + address_x0 + '"]/text()')
+            if len(address) > 0:
+                item["address"]=address[0].strip()
+
+            prop_type = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + type_x0 + '"]/text()')
+            if len(prop_type) > 0:
+                item["type"]=prop_type[0].strip()
+
+            price = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][substring(@x1,1,5)="' + price_x1 + '"]/text()')
+            if len(price) > 0:
+                item["price"]=price[0].strip()
+
+            result = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + result_x0 + '"]/text()')
+            if len(result) > 0:
+                item["result"]=result[0].strip()
+
+            agent = \
+                page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + agent_x0 + '"]/text()')
+            if len(agent) > 0:
+                item["agent"]=agent[0].strip()
+
+            if "".join(empty_item.values()) == "".join(item.values()):
+                continue
+
+            # Write out to the sqlite database using scraperwiki library
+            scraperwiki.sqlite.save(unique_keys=['suburb','address'], data={
+                "suburb": item["suburb"],
+                "address": item["address"],
+                "type": item["type"],
+                "price": item["price"],
+                "result": item["result"],
+                "agent": item["agent"],
+                "extracted_on": extractedOn
+            })
+
+
 # Main scrapper logic
 file_name = "http://www.homepriceguide.com.au/saturday_auction_results/Melbourne_Domain.pdf"
 local_file = download_file(file_name)
@@ -49,77 +116,18 @@ pdf = pdfquery.PDFQuery(local_file)
 
 print "PDFQuery created"
 
-pdf.load()
+for page_num in range(1,250):
 
-print "PDF loaded into memory"
+    try:
+        pdf.load(page_num)
+    except:
+        print "Done"
+        break
 
-# temporary loading from  file
-#root = etree.parse("f1.xml")
-root = pdf.tree
+    print "PDF page %s loaded into memory" % page_num
 
-# for every page determine column coordinates and parse values
-for page in root.xpath("/pdfxml/LTPage"):
-    suburb_x0 = page.xpath('.//LTTextLineHorizontal[substring(text(),1,6)="Suburb"][1]/@x0')[0]
-    address_x0 = page.xpath('.//LTTextLineHorizontal[substring(text(),1,7)="Address"][1]/@x0')[0]
-    type_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,4)="Type"][1]/@x0')[0]
-    price_x1 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,5)="Price"][1]/@x1')[0][0:5]
-    result_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,6)="Result"][1]/@x0')[0]
-    agent_x0 = page.xpath('.//LTTextBoxHorizontal[substring(text(),1,5)="Agent"][1]/@x0')[0]
-    lines_y0 = set(page.xpath('.//*[string-length()>0]/@y0'))
+    root = pdf.tree
 
-    for line_y0 in lines_y0:
-
-        item = {
-            "suburb": "",
-            "address": "",
-            "type": "",
-            "price": "",
-            "result": "",
-            "agent": ""
-        }
-
-        empty_item = item.copy()
-
-        suburb = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + suburb_x0 + '"]/text()')
-        if len(suburb) > 0:
-            item["suburb"]=suburb[0].strip()
-
-        address = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + address_x0 + '"]/text()')
-        if len(address) > 0:
-            item["address"]=address[0].strip()
-
-        prop_type = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + type_x0 + '"]/text()')
-        if len(prop_type) > 0:
-            item["type"]=prop_type[0].strip()
-
-        price = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][substring(@x1,1,5)="' + price_x1 + '"]/text()')
-        if len(price) > 0:
-            item["price"]=price[0].strip()
-
-        result = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + result_x0 + '"]/text()')
-        if len(result) > 0:
-            item["result"]=result[0].strip()
-
-        agent = \
-            page.xpath('.//*[string-length(normalize-space(translate(text(),"\n","")))>0][@y0="' + line_y0 + '"][@x0="' + agent_x0 + '"]/text()')
-        if len(agent) > 0:
-            item["agent"]=agent[0].strip()
-
-        if "".join(empty_item.values()) == "".join(item.values()):
-            continue
-
-        # Write out to the sqlite database using scraperwiki library
-        scraperwiki.sqlite.save(unique_keys=['suburb','address'], data={
-            "suburb": item["suburb"],
-            "address": item["address"],
-            "type": item["type"],
-            "price": item["price"],
-            "result": item["result"],
-            "agent": item["agent"],
-            "extracted_on": extractedOn
-        })
+    # for every page determine column coordinates and parse values
+    for page in root.xpath("/pdfxml/LTPage"):
+        parse_page(page)
